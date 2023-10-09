741dbd7572f7fd91934f3b7a8e4ef9a83cd607c6
Udbhav Ghimire <ghimireudbhav@gmail.com>
2023-10-09 10:56:37 +0545

    sav max email added

--- contactForm.php
+++ contactForm.php
@@ -20,7 +20,7 @@ $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587; // TCP port to connect to
 
 $mail->setFrom('info@bramptonduo.ca', $_POST['name']);
-// $mail->addAddress('info@savemaxglobalrealty.com');
+$mail->addAddress('info@savemaxglobalrealty.com');
 $mail->addAddress('milan@homebaba.ca');
 ;
 
